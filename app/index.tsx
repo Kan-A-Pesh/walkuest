e97e1f009fb1e9fc2b18015bac578f259c000185
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-09-23 00:59:15 +0200

    ✏️ Refactor MapView configuration to use OSM

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { View } from "react-native";
 import dark from "../styles/maps/dark";
-import MapView, { UrlTile } from "react-native-maps";
+import MapView, { MapUrlTile, UrlTile } from "react-native-maps";
 
 export interface AppProps {}
 
@@ -10,11 +10,24 @@ export default function App(props: AppProps) {
         <View style={{ flex: 1 }}>
             <MapView
                 style={{ flex: 1 }}
+                mapType="none"
                 userInterfaceStyle="dark"
+                camera={{
+                    heading: 0,
+                    center: { latitude: 37.78825, longitude: -122.4324 },
+                    pitch: 0,
+                }}
+                cameraZoomRange={{
+                    minCenterCoordinateDistance: 300,
+                }}
+                pitchEnabled={false}
+                toolbarEnabled={false}
                 userLocationPriority="high"
                 showsUserLocation={true}
                 customMapStyle={dark}
-            ></MapView>
+            >
+                <MapUrlTile urlTemplate="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" minimumZ={0} shouldReplaceMapContent={true} />
+            </MapView>
         </View>
     );
 }
